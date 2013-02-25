257eb38085538521f2818723f477ab7d79fb9a27
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2013-02-25 18:54:28 -0500

    Initial reader dumps raw toString to screen

--- android/src/com/example/kanbandatatracker/MainActivity.java
+++ android/src/com/example/kanbandatatracker/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.kanbandatatracker;
 
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +22,22 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+	        Parcelable[] rawMsgs = getIntent().getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+	        if (rawMsgs != null) {
+	            NdefMessage[] msgs = new NdefMessage[rawMsgs.length];
+	            for (int i = 0; i < rawMsgs.length; i++) {
+	                msgs[i] = (NdefMessage) rawMsgs[i];
+	                final TextView helloWorldText = (TextView) findViewById(R.id.hello_world);
+	                helloWorldText.setText(msgs[i].toString());
+	            }
+	        }
+	    }
 	}
 
 }
