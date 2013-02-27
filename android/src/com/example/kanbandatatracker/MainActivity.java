bdce61c4692e083d51e5c7a77a088481c3efa047
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2013-02-26 19:17:27 -0500

    Refactored code for reusability

--- android/src/com/example/kanbandatatracker/MainActivity.java
+++ android/src/com/example/kanbandatatracker/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.kanbandatatracker;
 
 import java.nio.charset.Charset;
+import java.util.LinkedList;
+import java.util.List;
 
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
@@ -13,41 +15,72 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-            Parcelable[] rawMsgs = getIntent().getParcelableArrayExtra(
-                    NfcAdapter.EXTRA_NDEF_MESSAGES);
-            if (rawMsgs != null) {
-                for (Parcelable rawMessage : rawMsgs) {
-                    NdefMessage message = (NdefMessage) rawMessage;
+	@Override
+	public void onResume() {
+		super.onResume();
+		List<String> payloads = tagToStrings();
+		
+		for (String text : payloads) {
+			final TextView helloWorldText = (TextView) findViewById(R.id.hello_world);
+		    helloWorldText.setText(text);
+		}
+	}
 
-                    NdefRecord[] records = message.getRecords();
-                    for (NdefRecord record : records) {
-                        byte[] payload = record.getPayload();
-                        String text = new String(payload,
-                                Charset.forName("US-ASCII"));
+	private List<String> tagToStrings() {
+		List<String> textPayloads = new LinkedList<String>();
+		
+		if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+			Parcelable[] rawMsgs = getIntent().getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+			textPayloads.addAll(rawToStrings(rawMsgs));
+	    }
+		
+		return textPayloads;
+	}
 
-                        final TextView helloWorldText = (TextView) findViewById(R.id.hello_world);
-                        helloWorldText.setText(text);
-                    }
-                }
-            }
-        }
-    }
+	private List<String> rawToStrings(Parcelable[] rawMsgs) {
+		List<String> textPayloads = new LinkedList<String>();
+		
+		if (rawMsgs != null) {
+		    for (Parcelable rawMessage : rawMsgs) {
+		        NdefMessage message = (NdefMessage) rawMessage;
+
+		        textPayloads.addAll(messageToStrings(message));
+		    }
+		}
+		
+		return textPayloads;
+	}
+
+	private List<String> messageToStrings(NdefMessage message) {
+		List<String> textPayloads = new LinkedList<String>();
+		
+		NdefRecord[] records = message.getRecords();
+		for (NdefRecord record : records) {
+			String text = decodePayload(record);
+			
+			textPayloads.add(text);
+		}
+		
+		return textPayloads;
+	}
+
+	private String decodePayload(NdefRecord record) {
+		byte[] payload = record.getPayload();
+		String text = new String(payload, Charset.forName("US-ASCII"));
+		return text;
+	}
 
 }
