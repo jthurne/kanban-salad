58f9aec50fe428f617a3cb60556dd0c65fe68de4
Jim Hurne <hurne_jim@yahoo.com>
2013-03-02 21:49:58 -0500

    Created a context menu for selected scanned items

--- android-scanner-client/src/main/java/org/kdt/ScanFragment.java
+++ android-scanner-client/src/main/java/org/kdt/ScanFragment.java
@@ -22,9 +22,14 @@ import org.kdt.kanbandatatracker.R;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -34,6 +39,9 @@ public class ScanFragment extends Fragment {
     private ArrayAdapter<String> scanLog;
 
     private View rootView;
+    private ListView scanLogView;
+
+    private ActionMode mActionMode;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,21 +50,6 @@ public class ScanFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_scan, container, false);
 
         initScanLog();
-        // @Override
-        // public void onSaveInstanceState(Bundle outState) {
-        // super.onSaveInstanceState(outState);
-        // outState.putStringArrayList("scanLog", scanLogList);
-        // }
-        //
-        // @Override
-        // public void onActivityCreated(Bundle savedInstanceState) {
-        // super.onActivityCreated(savedInstanceState);
-        // if (savedInstanceState == null)
-        // return;
-        //
-        // scanLog.clear();
-        // scanLog.addAll(savedInstanceState.getStringArrayList("scanLog"));
-        // }
         return rootView;
     }
 
@@ -65,9 +58,16 @@ public class ScanFragment extends Fragment {
         scanLog = new ArrayAdapter<String>(this.getActivity(),
                 R.layout.scanned_item, scanLogList);
 
-        ListView scanLogView = (ListView) rootView.findViewById(R.id.scanLog);
-
+        scanLogView = (ListView) rootView.findViewById(R.id.scanLog);
         scanLogView.setAdapter(scanLog);
+
+        enableContextActionBarWhenSelectingScannedItems(scanLogView);
+    }
+
+    private void enableContextActionBarWhenSelectingScannedItems(
+            ListView scanLogView) {
+        scanLogView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        scanLogView.setOnItemClickListener(new ItemSelectedListener());
     }
 
     public void appendToLog(String textToDisplay) {
@@ -104,4 +104,59 @@ public class ScanFragment extends Fragment {
         super.onResume();
         Log.v("ScanFragment", "onResume");
     }
+
+    private class ItemSelectedListener implements
+            AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position,
+                long id) {
+            if (mActionMode != null) {
+                return;
+            }
+            mActionMode = getActivity().startActionMode(
+                    new ActionModeCallback());
+        }
+    }
+
+    private class ActionModeCallback implements ActionMode.Callback {
+
+        // Called when the action mode is created; startActionMode() was called
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.scanned_item, menu);
+            return true;
+        }
+
+        // Called each time the action mode is shown. Always called after
+        // onCreateActionMode, but
+        // may be called multiple times if the mode is invalidated.
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+            case R.id.action_delete_scan:
+                mode.finish(); // Action picked, so close the CAB
+                return true;
+            case R.id.action_program_scan:
+                mode.finish(); // Action picked, so close the CAB
+                return true;
+            default:
+                return false;
+            }
+        }
+
+        // Called when the user exits the action mode
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            int selectedItem = scanLogView.getCheckedItemPosition();
+            scanLogView.setItemChecked(selectedItem, false);
+            mActionMode = null;
+        }
+    };
 }
