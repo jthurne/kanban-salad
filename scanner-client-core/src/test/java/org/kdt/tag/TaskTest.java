dbee257a8e8ab41e471e1b2a993e536b13eb89b2
Jim Hurne <hurne_jim@yahoo.com>
2013-04-19 21:33:48 -0400

    Renamed the model test package to tag

--- scanner-client-core/src/test/java/org/kdt/tag/TaskTest.java
+++ scanner-client-core/src/test/java/org/kdt/tag/TaskTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kdt.model;
+package org.kdt.tag;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
